e4dd66e89a0195f31e71e13ef8b67a6432c47559
rakib3004 <bsse1129@iit.du.ac.bd>
2021-10-14 10:25:39 +0600

    using spread operator print the list of number and add more number and print them

---
+++ spreadOperator.js
@@ -0,0 +1,17 @@
+let example = [1,2,3,4,5,6]
+
+let fetch = [...example];
+
+console.log(fetch)
+
+fetch.push(6)
+fetch.push(7)
+fetch.push(2)
+
+console.log(fetch)
+
+fetch.push(6)
+fetch.push(7)
+fetch.push(2)
+
+console.log(fetch)
