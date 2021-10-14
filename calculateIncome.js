39c2395792a233c069d9274e48d8568fdbba57af
rakib3004 <bsse1129@iit.du.ac.bd>
2021-10-14 09:29:24 +0600

    calculate total income from list of income

---
+++ calculateIncome.js
@@ -0,0 +1,14 @@
+let incomes = [62000, 67000, 55000];
+let total = 0;
+
+console.log('List of Income : ')
+
+for (const income of incomes){
+console.log(income);
+
+total+=income;
+
+}
+
+console.log('-------------------');
+console.log('Total Income is: '+total);
