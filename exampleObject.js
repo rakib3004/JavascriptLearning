5d5569cec328936b453974960a58230030851aef
rakib3004 <bsse1129@iit.du.ac.bd>
2021-10-14 10:55:23 +0600

    print out all object value

---
+++ exampleObject.js
@@ -0,0 +1,12 @@
+const example = {};
+
+example.firstName = 'Roushon';
+example.lastName= 'Jahan';
+example.state = 'Mymensign';
+example.city= 'Jamalpur';
+example.department = 'Computer Science';
+example.gpa= '4.83';
+example.institute = 'Engineering College';
+example.designation= 'CTO';
+
+console.log(example)
