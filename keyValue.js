3e653064138251956f1bb02ec55d655025b28297
rakib3004 <bsse1129@iit.du.ac.bd>
2021-10-14 09:08:26 +0600

    key-value implemantation in list in javascript

---
+++ keyValue.js
@@ -0,0 +1,4 @@
+let [structural, objectOriented, sciptingLanguage,markupLanguage, sql] = ['c-programming', 'java-programming', 'python-programming', 'html-programming', 'MySQL'];
+console.log('My most favourite two languages are '+objectOriented+ ' and '+ sciptingLanguage);
+console.log(sql+ ' and '+ markupLanguage+ ' are most essental language for web programming');
+console.log(structural+' is the most powerful language ever in computer science  history');
