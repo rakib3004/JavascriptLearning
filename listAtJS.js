cb44f197c60a59962ebab70aa224ddf5376f47c8
rakib3004 <bsse1129@iit.du.ac.bd>
2021-10-14 09:01:33 +0600

    implement list in javascript

---
+++ listAtJS.js
@@ -0,0 +1,6 @@
+let city = ['Dhaka', 'Rajshahi', 'Khulna', 'Chottrogram'];
+console.log(city);
+console.log("First City Name is: "+ city[0]);
+console.log("Second City Name is: "+ city[1]);
+console.log("Third City Name is: "+ city[2]);
+console.log("Forth City Name is: "+ city[3]);
