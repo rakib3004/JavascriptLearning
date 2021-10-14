e691c5eeb5322115a8a2e2e8849b74c9d22dd578
rakib3004 <bsse1129@iit.du.ac.bd>
2021-10-14 08:58:36 +0600

    print name from javascript structure

---
+++ personalInformation.js
@@ -0,0 +1,13 @@
+const personalInformation = {
+
+firstName : 'Muktadir',
+lastName  : 'Ahmed',
+city : 'Dhanmondi',
+state : 'Dhaka',
+zipCode : '1209',
+
+};
+
+const {firstName: fn, lastName: ln} = personalInformation;
+
+console.log(`${fn} ${ln}`)
